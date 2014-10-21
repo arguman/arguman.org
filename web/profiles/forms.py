e117b517098dea12e939d6b28e6142d3dfc4e818
fatiherikli <fatih@hipo.biz>
2014-10-21 22:15:57 +0300

    Fix clean_username method

--- web/profiles/forms.py
+++ web/profiles/forms.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
 from profiles.models import Profile
 
 
@@ -6,3 +8,17 @@ class RegistrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ("username", "email")
         model = Profile
+
+
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            Profile._default_manager.get(username=username)
+        except Profile.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
