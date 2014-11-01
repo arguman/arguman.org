260ca77ad4fb0a0bab91b5d4dad89af0105c3169
Berker Peksag <berker.peksag@gmail.com>
2014-11-01 19:11:32 +0200

    Use JsonResponse instead of HttpResponse(json.dumps()).

--- web/profiles/mixins.py
+++ web/profiles/mixins.py
@@ -1,7 +1,4 @@
-import json
-
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 
 
@@ -12,26 +9,3 @@ class LoginRequiredMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
-
-
-class JSONResponseMixin(object):
-    """
-    A mixin that can be used to render a JSON response.
-    """
-    response_class = HttpResponse
-
-    def render_to_response(self, context, **response_kwargs):
-        """
-        Returns a JSON response, transforming 'context' to make the payload.
-        """
-        response_kwargs['content_type'] = 'application/json'
-        return self.response_class(
-            self.convert_context_to_json(context),
-            **response_kwargs
-        )
-
-    def convert_context_to_json(self, context):
-        """
-        Convert the context dictionary into a JSON object
-        """
-        return json.dumps(context)
