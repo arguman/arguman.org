a1879863e55d2b2997815e6ccff04cbd6243dea2
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-01-08 23:19:18 +0200

    add api app

--- web/api/v1/urls.py
+++ web/api/v1/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^v1/', include('api.v1.urls', namespace='api')),
+    url(r'^auth/', include('api.v1.auth.urls', namespace='api-auth')),
 )
