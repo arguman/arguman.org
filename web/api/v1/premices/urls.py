87c42e494463d3a7e1c20e5dfdd9d30e3352b76f
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-01-15 21:46:33 +0200

    editing

---
+++ web/api/v1/premices/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns(
+    '',
+)
--- web/api/v1/urls.py
+++ web/api/v1/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^auth/', include('api.v1.auth.urls', namespace='api-auth')),
+    url(r'^auth/', include('api.v1.auth.urls',
+                           namespace='api-auth')),
+    url(r'^premices/', include('api.v1.premices.urls',
+                               namespace='api-premices')),
 )
