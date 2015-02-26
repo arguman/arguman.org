36e5a23c447bb3fde63bdb62af49244a0cb67c08
Bahattin Cinic <bahattin.cinic@metglobal.com>
2015-02-26 21:26:05 +0200

    remove unused methods

--- web/api/v1/notifications/serializers.py
+++ web/api/v1/notifications/serializers.py
@@ -9,12 +9,8 @@ class NotificationSerializer(serializers.ModelSerializer):
     recipient = UserProfileSerializer()
     notification_type = serializers.ReadOnlyField(
         source='get_notification_type_display')
-    target_object_type = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
         fields = ('sender', 'recipient', 'date_created', 'notification_type',
                   'is_read', 'target_object_id', 'id',)
-
-    def target_object_type(self, obj):
-        return obj.get_target_type().__name__
